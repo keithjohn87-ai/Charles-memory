ff4351e7152a98bcf94f3b33d80545047c1a5247
Charles <charles@local>
2026-05-06 19:12:48 -0400

    self_modify: Register current_time tool

--- tools/__init__.py
+++ tools/__init__.py
@@ -1,2 +1,2 @@
 """Importing this package registers all tools via @tool decorators."""
-from . import filesystem, memory, self_modify, shell  # noqa: F401
+from . import filesystem, memory, self_modify, shell, time  # noqa: F401
